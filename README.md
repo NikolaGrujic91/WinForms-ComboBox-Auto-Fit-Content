7f6d664a48429036cfdf17f712775a48e4e4b070
Nikola Grujić <grujic.nikola91@gmail.com>
2020-04-02 10:40:58 +0200

    Added project and updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# WinForms-ComboBox-Auto-Fit-Content
+# WinForms ComboBox Auto Fit Content
+
+This repository contains implementation of ComboBox dropdown auto fitting content.
+
+[WinForms Examples](https://github.com/NikolaGrujic91/WinForms-Examples)
+
+![image missing](App.png "Application UI")
---
+++ WinFormsComboBoxAutoFitContent/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsComboBoxAutoFitContent
+{
+    public partial class Form1 : Form
+    {
+        #region Constructors
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var dataSource1 = new string[] {"Item1" , "Very, very long string for example", "Item3", "Item4"};
+            this.comboBox1.DataSource = dataSource1;
+            this.comboBox1.DropDownWidth = this.DropDownWidth(dataSource1);
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private int DropDownWidth(string[] data)
+        {
+            int maxWidth = 0;
+            var label = new Label();
+
+            foreach (string item in data)
+            {
+                label.Text = item;
+
+                if (label.PreferredWidth > maxWidth)
+                {
+                    maxWidth = label.PreferredWidth;
+                }
+            }
+
+            label.Dispose();
+            return maxWidth;
+        }
+
+        #endregion
+    }
+}
